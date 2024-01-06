8a1ea7b741cc950612aa6d94cbbf5c3412eedfe2
sudip <sudip@reg-1.com>
2024-01-06 14:59:43 +0545

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect  
+from myapp.forms import EmployeeForm  
+from myapp.models import Employee  
+from django.http import HttpResponse
+ 
+# Create your views here.  
+def testpage(request):
+    cc=[{"aasdf":"qwer"},{"aasdf":"qwer"}]
+    return render(request,"test.html",{"cc":cc})
+    # return HttpResponse(cc)
+
+def addnew(request):  
+    if request.method == "POST":  
+        form = EmployeeForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/')  
+            except:  
+                pass
+    else:  
+        form = EmployeeForm()  
+    return render(request,'index.html',{'form':form})  
+ 
+def index(request):  
+    employees = Employee.objects.all()  
+    return render(request,"show.html",{'employees':employees})  
+ 
+def edit(request, id):  
+    employee = Employee.objects.get(id=id)  
+    return render(request,'edit.html', {'employee':employee})  
+ 
+def update(request, id):  
+    employee = Employee.objects.get(id=id)  
+    form = EmployeeForm(request.POST, instance = employee)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/")  
+    return render(request, 'edit.html', {'employee': employee})  
+     
+def destroy(request, id):  
+    employee = Employee.objects.get(id=id)  
+    employee.delete()  
+    return redirect("/")  
